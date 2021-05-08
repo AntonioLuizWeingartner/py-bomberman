52ca896ca4b2cbe5e688a1650eb5178043f207b8
Antônio Luiz Weingartner Junior <antonioluiz686@gmail.com>
2021-05-08 19:05:18 -0300

    Adicionado módulo de matemática

---
+++ core/math.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+class Vector2:
+
+    def __init__(self, x: float, y: float):
+        self.x = x;
+        self.y = y
+
+    def __add__(self, other: Vector2):
+        return Vector2(self.x + other.x, self.y + other.y)
+    
+    def __iadd__(self, other: Vector2):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __sub__(self, other: Vector2):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __isub__(self, other: Vector2):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __mul__(self, other: float):
+        return Vector2(self.x*other, self.y*other)
+
+    def __imul__(self, other: float):
+        self.x *= other
+        self.y *= other
+        return self
+    
+    def __repr__(self) -> str:
+        return "Vector2 (x={}, y={})".format(self.x, self.y)
