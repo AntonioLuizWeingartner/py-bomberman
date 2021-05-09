065f02f5a3dc5d29d33249f2a79677e4f306da89
Antônio Luiz Weingartner Junior <antonioluiz686@gmail.com>
2021-05-09 02:46:54 -0300

    Módulo game_components.py adicionado. Adicionado classes GameGrid, GridAgent, AIController, PlayerController, GameManager

---
+++ core/game_components.py
@@ -0,0 +1,27 @@
+import core.entity_system
+
+class GameGrid(core.entity_system.ScriptableComponent):
+
+    def update(self):
+        pass
+
+
+class GridAgent(core.entity_system.ScriptableComponent):
+    
+    def update(self):
+        pass
+
+class AIController(core.entity_system.ScriptableComponent):
+    
+    def update(self):
+        pass
+
+class PlayerController(core.entity_system.ScriptableComponent):
+
+    def update(self):
+        pass
+
+class GameManager(core.entity_system.ScriptableComponent):
+
+    def update(self):
+        pass
